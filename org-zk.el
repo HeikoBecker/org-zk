ab7a49d723b07d6ca7d460548134b3f6e03c04b2
Heiko Becker <hbecker@mpi-sws.org>
2020-02-28 12:01:07 +0100

    Make variables and templates customizable, include title in filename

--- org-zk.el
+++ org-zk.el
@@ -1,8 +1,20 @@
-(defvar org-zk-dir "~/Zettelkasten/"
+(defcustom org-zk-dir "~/Zettelkasten/"
   "Default Zettelkasten directory.")
 
-(defvar org-zk-append-capture t
+(defcustom org-zk-append-capture t
   "If non-nil, append a zettelkasten capture template to variable org-capture-templates")
+
+(defun org-zk-visit-timestamped-file-with-title ()
+  "Visit a new file named by the current timestamp"
+  (interactive)
+  (setq new-zettel--name (read-string "Title:"))
+  (let* (
+        (curr-date-stamp (format-time-string "%Y%m%d-%H%M"))
+        (safe-file-name (replace-regexp-in-string " " "-" new-zettel--name))
+        (file-string (format "%s-%s.org" curr-date-stamp safe-file-name))
+        (file-name (expand-file-name file-string org-zk-dir)))
+  (set-buffer (org-capture-target-buffer file-name))
+  (goto-char (point-max))))
 
 (defun org-zk-visit-timestamped-file ()
   "Visit a new file named by the current timestamp"
@@ -19,21 +31,34 @@
   (grep-compute-defaults)
   (rgrep content "*.org" org-zk-dir))
 
+(setq org-zk-plain-note-template
+      "%(format \"#+TITLE: %s\n\" new-zettel--name)\
+#+PROPERTY: Timestamp %<%Y%m%d-%H%M>\n\n%?")
+
+(setq org-zk-paper-note-template
+      (concat org-zk-plain-note-template
+              "#+PROPERTY: BIBKEY %^{bibtexkey}\n\
+#+PROPERTY: Tags %^{tags}\n\n\
+%?* General Information\n\
+** Category\n\
+** Context (Related work and theoretical bases)\n\
+** Correctness\n** Contributions\n** Clarity"))
+
+(defun get-org-zk-plain-template ()
+  (format "%s" org-zk-plain-note-template))
+
+(defun get-org-zk-paper-template ()
+  (format "%s" org-zk-paper-note-template))
+
 ;; Append a Zettelkasten capture for org-capture
 (when org-zk-append-capture
   (setq org-capture-templates
     (append org-capture-templates
             '(
               ("Z" "Zettel [Note]" plain
-               (function org-zk-visit-timestamped-file)
-               "#+TITLE: %^{TITLE}\n#+PROPERTY: Timestamp %<%Y%m%d_%H%M>\n\n %?")
+               (function org-zk-visit-timestamped-file-with-title)
+               (function get-org-zk-plain-template))
+               ;;"#+TITLE: %^{TITLE}\n#+PROPERTY: Timestamp %<%Y%m%d_%H%M>\n\n %?")
               ("P" "Paper [Zettelkasten]" plain
                (function org-zk-visit-timestamped-file)
-               "#TITLE: %^{Title}\n#+PROPERTY: Timestamp %<%Y%m%d_%H%M>\n\
-#+PROPERTY: BIBKEY %^{bibtexkey}\n\
-#+TAGS:\n\n\
-%?* General Information\n\
-** Category\n\
-** Context (Related work and theoretical bases)\n\
-** Correctness\n** Contributions\n** Clarity")
-              ))))
+               (function get-org-zk-paper-template))))))
